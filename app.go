74c76066519546930de26a40457a9a5693b645a3
SEB534542 <shj.vandermeulen@gmail.com>
2020-06-27 12:09:45 +0200

    Added all todo's

--- app.go
+++ app.go
@@ -3,6 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	//test
 	fmt.Println("Welkom")
+	//TODO: Set vars
+	//TODOD: configure GPIO
+	//TODO: init: move sunscreen (up)
+	//TODO: add cases: time < sunrise / time > sunset / x minutes before sunset
+	//TODO: measure light
+	//TODO: add cases: sunscreen up / down vs weather
+	//TODO: defer: GPIO clean-up + move sunscreen
 }
