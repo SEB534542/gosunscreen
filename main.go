8fe442092de7f2c14698a36af83ce3ee6e30802e
SEB534542 <shj.vandermeulen@gmail.com>
2020-06-29 20:21:27 +0200

    Added sunscreen struct

--- main.go
+++ main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"strconv"
 	"time"
 )
 
-const sunriseTime string = "10:00" // prefered sunrise time in "hh:mm"
-const sunsetTime string = "18:15"  // prefered sunset time in "hh:mm"
-const sunscreenDown int = 17       // seconds
-const sunscreenUp int = 20         // seconds
-const sunsetThreshold int = 70     // minutes before sunset that sunscreen no longer should go down
-const interval int = 60            // interval for checking current light in seconds
+type sunscreen struct {
+	sunrise  time.Time // Time after which sunscreen can shine on the sunscreen area
+	sunset   time.Time // Time after which sunscreen no can shine on the sunscreen area
+	secDown  int       // Seconds to move sunscreen down
+	secUp    int       // Seconds to move sunscreen up
+	position string    // Position of sunscreen
+	pinDown  int       // GPIO pin for moving sunscreen down
+	pinUp    int       // GPIO pin for moving sunscreen up
+}
+
+const sunsetThreshold int = 70 // minutes before sunset that sunscreen no longer should go down
+const interval int = 60        // interval for checking current light in seconds
 
 const lightGoodValue int = 9      // max measured light value that counts as "good weather"
 const ligthGoodThreshold int = 15 // number of times light should be below lightGoodValue
@@ -23,31 +31,82 @@ const ligthBadThreshold int = 15 // number of times light should be above lightB
 
 const allowedOutliers int = 2 // number of outliers accepted in the measurement
 
-const pinSuncreenUp int = 38    // pin for moving sunscreen up
-const pinSunscreenDown int = 40 // pin for moving sunscreen down
-const pinSunlight int = 16      // pin for retrieving light value
+const pinSunlight int = 16 // pin for retrieving light value
+
+var sunscreenStatus = "unknown"
 
 func main() {
+	s1 := sunscreen{
+		sunrise:  stoTime("10:00", 0),
+		sunset:   stoTime("23:00", 0),
+		secDown:  17,
+		secUp:    20,
+		position: "unknown",
+		pinDown:  40,
+		pinUp:    38,
+	}
+	if sunscreenStatus == "unknown" {
+		moveSunscreen("up")
+	}
 
-	sunrise := setTime(sunriseTime, 1)
-	sunset := setTime(sunsetTime, 0)
+	light := []int{}
 
-	fmt.Println(sunrise)
-	fmt.Println(sunset)
+	switch {
+	case s1.sunset.Sub(time.Now()) < 0:
+		log.Printf("Sun is down (%v), adjusting sunrise/set to tomorrow", s1.sunset)
+		//TODO: adjust time by one day
+		fallthrough
+	case s1.sunrise.Sub(time.Now()) > 0:
+		log.Printf("Sun is not yet up (%v), snoozing", s1.sunrise)
+		// sleep until sunrise
+		fallthrough
+	default:
+		fmt.Println("Sunrise:", s1.sunrise)
+		fmt.Println("Sunset:", s1.sunset)
+		light = append(light, getLight(pinSunlight))
+		fmt.Println("Light is:", light)
+	}
 
-	//TODO: Set vars using func init()
 	//TODO: configure GPIO
 	//TODO: init: move sunscreen (up)
-	//TODO: add cases: time < sunrise / time > sunset / x minutes before sunset
-	//TODO: measure light
+	//TODO: add logic: x minutes before sunset
 	//TODO: add cases: sunscreen up / down vs weather
 	//TODO: defer: GPIO clean-up + move sunscreen
 	//TODO: add keyboard interrupt
 
 }
 
-// Based on the received string time (format hh:mm) and the offset, the func returns a type time with today's today + the offset
-func setTime(t string, d int) time.Time {
-	timeNow := time.Now().Day()
-	return time.Date(timeNow.Year, timeNow.Month, timeNow.Day+d, int(t[:2]), int(sunriseTime), 0, 0, time.Local)
+// Based on the received string time (format hh:mm) and the day offset, the func returns a type time with today's date + the offset in days
+func stoTime(t string, days int) time.Time {
+	timeNow := time.Now()
+
+	timeHour, err := strconv.Atoi(t[:2])
+	if err != nil {
+		log.Panicf("Time %v is not correctly formatted. Please unsure time is written as hh:mm. Error: %v", t, err)
+	}
+
+	timeMinute, err := strconv.Atoi(t[3:])
+	if err != nil {
+		log.Panicf("Time %s is not correctly formatted. Please unsure time is written as hh:mm", t)
+	}
+
+	return time.Date(timeNow.Year(), timeNow.Month(), timeNow.Day()+days, int(timeHour), int(timeMinute), 0, 0, time.Local)
+}
+
+// Measure light from specified GPIO pin and return value
+func getLight(pin int) int {
+	// TODO: measure ligth
+	fmt.Println("Light pin:", pin)
+	return 5
+}
+
+//
+func moveSunscreen(direction string) {
+	// Include error if direction is not up or down
+}
+
+func close() {
+	if sunscreenStatus != "up" {
+		moveSunscreen("up")
+	}
 }
--- main_test.go
+++ main_test.go
@@ -1,13 +1,31 @@
 package main
 
-import "testing"
-
-func TestMyAdd(t *testing.T) {
-	if myAdd(2, 3) != 2+3 {
-		t.Error("Expected", 5, "Got", myAdd(2, 3))
-	}
-}
+import (
+	"testing"
+	"time"
+)
 
 func TestSetTime(t *testing.T) {
+	type test struct {
+		dataTime  string
+		dataDelta int
+		answer    time.Time
+	}
 
+	tests := []test{
+		{"10:00", 1, time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day()+1, 10, 0, 0, 0, time.Local)},
+		{"12:30", 0, time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), 12, 30, 0, 0, time.Local)},
+		{"13:21", -1, time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day()-1, 13, 21, 0, 0, time.Local)},
+		{"13:01", -1, time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day()-1, 13, 1, 0, 0, time.Local)},
+		{"09:01", -1, time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day()-1, 9, 1, 0, 0, time.Local)},
+		{"00:00", 0, time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), 0, 0, 0, 0, time.Local)},
+	}
+
+	for _, v := range tests {
+		x := stoTime(v.dataTime, v.dataDelta)
+
+		if x != v.answer {
+			t.Error("Expected", v.answer, "Got", x)
+		}
+	}
 }
