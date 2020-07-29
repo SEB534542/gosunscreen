4fddb7c284b5d631e804458dfbdcaebcdd7c7af6
SEB534542 <shj.vandermeulen@gmail.com>
2020-07-29 18:08:17 +0200

    adjust variables

--- website.go
+++ website.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"time"
+	//"fmt"
 	//"io"
 )
 
@@ -19,8 +19,8 @@ const manual string = "manual"
 const up string = "up"
 const down string = "down"
 
-var sunrise = "10:00"
-var sunset = "18:45"
+var Sunrise = "10:00"
+var Sunset = "18:45"
 
 var tpl *template.Template
 var s1 *sunscreen = &sunscreen{Mode: autom, Position: up}
@@ -57,31 +57,42 @@ func modeHandler(w http.ResponseWriter, req *http.Request) {
 	switch mode {
 	case autom:
 		s1.Mode = autom
-		fmt.Println("New Mode:", s1.Mode, "// New Position:", s1.Position)
+		log.Println("Updated Mode:", s1.Mode, "and Position:", s1.Position)
 	case manual + "/" + up:
 		s1.Mode = manual
 		s1.Position = up
-		fmt.Println("New Mode:", s1.Mode, "// New Position:", s1.Position)
+		log.Println("Updated Mode:", s1.Mode, "and Position:", s1.Position)
 	case manual + "/" + down:
 		s1.Mode = manual
 		s1.Position = down
-		fmt.Println("New Mode:", s1.Mode, "// New Position:", s1.Position)
+		log.Println("Updated Mode:", s1.Mode, "and Position:", s1.Position)
 	default:
-		fmt.Println("Unknown mode:", req.URL.Path)
-		fmt.Println("Current Mode:", s1.Mode, "// Current Position:", s1.Position)
+		log.Println("Unknown mode:", req.URL.Path)
+		log.Println("Current Mode:", s1.Mode, "// Current Position:", s1.Position)
 	}
 	http.Redirect(w, req, "/", http.StatusFound)
 }
 
-func configHandler(w http.ResponseWriter, req *http.Request) {	
+func configHandler(w http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if len(req.PostForm) != 0 {
+		Sunrise = req.PostForm["Sunrise"][0]
+		Sunset = req.PostForm["Sunset"][0]
+		log.Println("Updated Sunrise:", Sunrise, "and Sunset:", Sunset)
+	}
+
 	data := struct {
 		Sunrise string
-		Sunset string
+		Sunset  string
 	}{
-		sunrise,
-		sunset,
+		Sunrise,
+		Sunset,
 	}
-	err := tpl.ExecuteTemplate(w, "config.gohtml", data)
+	err = tpl.ExecuteTemplate(w, "config.gohtml", data)
 	if err != nil {
 		log.Fatalln(err)
 	}
