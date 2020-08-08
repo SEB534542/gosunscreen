385f8f579cc40c17d57df2aba3c4f1ec33bb294a
SEB534542 <shj.vandermeulen@gmail.com>
2020-08-08 10:01:28 +0200

    Append to csv

--- csvtest.go
+++ csvtest.go
@@ -1,37 +1,42 @@
 package main
 
 import (
-	"fmt"
-	"encoding/csv"
-	"log"
-	"os"
+    "encoding/csv"
+    "os"
+    "log"
 )
 
-var data = [][]string{{"Line1", "Hello Readers of"}, {"Line2", "golangcode.com"}}
-
 func main() {
-    file, err := os.Open("result.csv")
-    checkError("Cannot open file", err)
-    defer file.Close()
-	
-	records, err := r.ReadAll()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-fmt.Print(records)
-
-    writer := csv.NewWriter(file)
-    defer writer.Flush()
-
-    for _, value := range data {
-        err := writer.Write(value)
-        checkError("Cannot write to file", err)
-    }
-}
-
-func checkError(message string, err error) {
+	const file = "result.csv"
+    // read the file
+    f, err := os.Open(file)
     if err != nil {
-        log.Fatal(message, err)
+        log.Fatal(err)
     }
+    defer f.Close()
+    r := csv.NewReader(f)
+    lines, err := r.ReadAll()
+    if err != nil {
+        log.Fatal(err)
+    }
+    if err = f.Close(); err != nil {
+       log.Fatal(err)
+    }
+
+    // add data
+    var data = [][]string{{"Line1", "Hello Readers of"}, {"Line2", "golangcode.com"}}
+    log.Printf("%T", lines)
+    lines = append(lines, data...)
+ 
+    // write the file
+    f, err = os.Create(file)
+    if err != nil {
+        log.Fatal(err)
+    }
+    w := csv.NewWriter(f)
+    if err = w.WriteAll(lines); err != nil {
+        log.Fatal(err)
+        
+    }
+    
 }
